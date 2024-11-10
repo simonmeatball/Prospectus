50e93d05188cd648982012dbf85a368c50a5e9ba
jasonni <jasonni19@g.ucla.edu>
2024-11-09 22:13:03 -0800

    Helped setup parts the frontend such as the CSS and App.jsx files

---
+++ prospectus/client/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css"
+import React from 'react';
+
+function App() {
+    return (
+    <div className = "container">
+        <h1 className = "Heading">Welcome to Prospectus!</h1>
+    </div>)
+}
+
+export default App
