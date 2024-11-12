d9be4f576cd479cf35cbce34c108133143a039bc
simonmeatball <62194612+simonmeatball@users.noreply.github.com>
2024-11-11 21:38:16 -0800

    Added/installed Tailwind CSS and DaisyUI (https://tailwindcss.com/docs/guides/vite) (https://daisyui.com/) and reorganized the file structure of the client side -- adding src folder and components, pages, and styles within for better organization. Created a button component to test Tailwind CSS integration.

---
+++ prospectus/client/src/components/App.jsx
@@ -0,0 +1,12 @@
+import "../styles/App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div class="flex items-center justify-center">
+      <h1 class="Heading">Welcome to Prospectus!</h1>
+    </div>
+  );
+}
+
+export default App;
