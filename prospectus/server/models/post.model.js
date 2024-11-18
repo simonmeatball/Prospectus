191749bbc9eb55e152f6f7ffc42c14c332784c8c
aroy23 <arnavroy@ucla.edu>
2024-11-17 16:45:24 -0800

    Made the required tag false for userId for posts models

--- prospectus/server/models/post.model.js
+++ prospectus/server/models/post.model.js
@@ -5,7 +5,7 @@ const postSchema = new mongoose.Schema(
     userID: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true,
+      required: false,
     },
     name: {
       type: String,
