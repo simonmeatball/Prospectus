6e32bd6d2b28332ea0ae2e093094c6c60f183008
aroy23 <arnavroy@ucla.edu>
2024-11-17 17:08:54 -0800

    Added delete post functionality

--- prospectus/server/server.js
+++ prospectus/server/server.js
@@ -15,7 +15,6 @@ app.post("/api/posts", async (req, res) => {
       .status(400)
       .json({ success: false, message: "Fill in all data fields" });
   }
-
   const newPost = new Post(post);
   try {
     await newPost.save();
@@ -23,6 +22,17 @@ app.post("/api/posts", async (req, res) => {
   } catch (err) {
     console.error("Error saving post", err.message);
     res.status(500).json({ success: false, message: err.message });
+  }
+});
+
+app.delete("/api/posts/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log(`Delete request received for Post ID: ${id}`);
+  try {
+    await Post.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Post deleted" });
+  } catch (err) {
+    res.status(404).json({ success: false, message: "Post not found" });
   }
 });
 
