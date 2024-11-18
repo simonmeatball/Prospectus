4fa33db143bd9c9880b523459362735accdef104
William Shih <148172235+williamzshih@users.noreply.github.com>
2024-11-17 18:37:38 -0800

    Beginning of post page

--- prospectus/client/src/pages/main.jsx
+++ prospectus/client/src/pages/main.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "../components/App.jsx";
-import Button from "../components/Button.jsx";
+import Navbar from "../components/Navbar.jsx";
+import Profile from "../components/Profile.jsx"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-    <div class="flex items-center justify-center">
-      <Button />
-    </div>
+    <Navbar />
+    <Profile />
   </React.StrictMode>
 );
