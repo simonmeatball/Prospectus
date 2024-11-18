c2aeef3b06c11729f66d034ad2276dd7c5f3dc6e
William Shih <148172235+williamzshih@users.noreply.github.com>
2024-11-17 17:20:00 -0800

    Install Formik for forms

--- prospectus/client/src/components/Button.jsx
+++ prospectus/client/src/components/Button.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { CheckIcon } from 'lucide-react'
 
 function Button() {
   return (
     <button class="px-4 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 font-sans">
       Sign Up
+      <CheckIcon />
     </button>
   );
 }
