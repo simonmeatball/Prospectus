60810eafb0b7036cc5c9791afab91e7ec1b0b1c6
simonmeatball <62194612+simonmeatball@users.noreply.github.com>
2024-12-02 09:29:16 -0800

    Usernames are now linked to profiles on posts (both poster and commenters)

--- prospectus/client/src/pages/Navbar.jsx
+++ prospectus/client/src/pages/Navbar.jsx
@@ -1,108 +1,108 @@
-import React, { useState, useRef , useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import axios from "axios";
-
+import { API_BASE_URL } from "@/config";
 
 import Logo1 from "../images/logo1.png";
 
 export default function Navbar() {
-  const { isAuthenticated, logout , user} = useAuth();
+  const { isAuthenticated, logout, user } = useAuth();
   const navigate = useNavigate();
 
-  const [searchQuery, setSearchQuery] = useState(""); 
-  const [results, setResults] = useState([]); 
-  const [loading, setLoading] = useState(false); 
-  const [error, setError] = useState(null); 
-  const [showResults, setShowResults] = useState(false); 
+  const [searchQuery, setSearchQuery] = useState("");
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [showResults, setShowResults] = useState(false);
   const [focusedIndex, setFocusedIndex] = useState(-1);
 
   const resultContainer = useRef(null);
   const [debouncedSearchQuery, setDebouncedSearchQuery] = useState(searchQuery);
 
-  const debounceTimeoutRef = useRef(null); 
+  const debounceTimeoutRef = useRef(null);
 
- // const debouncedSearchQuery = useDebounce(searchQuery, 500); //500 ms delay before making search request 
+  // const debouncedSearchQuery = useDebounce(searchQuery, 500); //500 ms delay before making search request
 
-  const handleDebounce = (value) => { 
-    if (debounceTimeoutRef.current) { 
-      clearTimeout(debounceTimeoutRef.current); 
+  const handleDebounce = (value) => {
+    if (debounceTimeoutRef.current) {
+      clearTimeout(debounceTimeoutRef.current);
     }
 
-    debounceTimeoutRef.current = setTimeout(()=> { 
+    debounceTimeoutRef.current = setTimeout(() => {
       setDebouncedSearchQuery(value);
     }, 400);
   };
 
   const handleLogout = () => {
     logout();
-    navigate('/login');
+    navigate("/login");
   };
 
-  const handleSearchChange = (event) => { 
-      const query = event.target.value; 
-      setSearchQuery(query);
-      console.log(query); 
-      setShowResults(true);
-      handleDebounce(query);
+  const handleSearchChange = (event) => {
+    const query = event.target.value;
+    setSearchQuery(query);
+    console.log(query);
+    setShowResults(true);
+    handleDebounce(query);
 
-      if (!debouncedSearchQuery.trim()) {
-        setResults([]);
-        return; 
-      }
-
-      handleSearch();
-  }
-
-  const handleSearch = async() => { 
     if (!debouncedSearchQuery.trim()) {
-      setResults([]); 
-      return; 
+      setResults([]);
+      return;
+    }
+
+    handleSearch();
+  };
+
+  const handleSearch = async () => {
+    if (!debouncedSearchQuery.trim()) {
+      setResults([]);
+      return;
     }
 
     setLoading(true);
-    setError(null); 
+    setError(null);
     console.log("search initiated:", debouncedSearchQuery);
-    
-    try { 
-      const response = await axios.get("http://localhost:8080/api/posts");
-      if (response.data.success) { 
-        const allPosts = response.data.data
-        console.log("all posts fetched:", response.data.data);
-      
-      const filteredPosts = allPosts.filter((post) => {
-        const regex = new RegExp(`\\b${debouncedSearchQuery}`, 'i'); 
-        return regex.test(post.title.toLowerCase());}
-     );
-     
-     setResults(filteredPosts);
-     console.log(filteredPosts); 
-      }
-    } catch (err)  {
-      setError("failed to search results"); 
-    } finally { 
-      setLoading(false); 
-    }
-  }; 
 
-  useEffect(() => { 
-    if (debouncedSearchQuery) { 
-      handleSearch(); 
+    try {
+      const response = await axios.get(`${API_BASE_URL}/api/posts`);
+      if (response.data.success) {
+        const allPosts = response.data.data;
+        console.log("all posts fetched:", response.data.data);
+
+        const filteredPosts = allPosts.filter((post) => {
+          const regex = new RegExp(`\\b${debouncedSearchQuery}`, "i");
+          return regex.test(post.title.toLowerCase());
+        });
+
+        setResults(filteredPosts);
+        console.log(filteredPosts);
+      }
+    } catch (err) {
+      setError("failed to search results");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (debouncedSearchQuery) {
+      handleSearch();
     }
   }, [debouncedSearchQuery]);
 
   const handleResultClick = (postId) => {
     navigate(`/post/${postId}`);
     setShowResults(false);
-  }
+  };
 
-  const handleSelection = (index) => { 
+  const handleSelection = (index) => {
     if (results[index]) {
       handleResultClick(results[index]._id);
     }
   };
 
-  const renderItem = (item) => { 
+  const renderItem = (item) => {
     return (
       <div className="cursor-pointer hover:bg-black hover:bg-opacity-10 p-2">
         {item.title}
@@ -113,9 +113,11 @@ export default function Navbar() {
   return (
     <div className="navbar bg-white fixed top-0 z-10 w-full">
       <div className="flex-1">
-
-        <img className="w-16" src = {Logo1} draggable="false"/>
-        <Link className="btn btn-ghost text-amber-500 font-bold italic text-2xl tracking-wide" to="/home">
+        <img className="w-16" src={Logo1} draggable="false" />
+        <Link
+          className="btn btn-ghost text-amber-500 font-bold italic text-2xl tracking-wide"
+          to="/home"
+        >
           prospectus
         </Link>
       </div>
@@ -130,35 +132,36 @@ export default function Navbar() {
             type="text"
             placeholder="Search"
             className="input input-bordered w-24 md:w-auto"
-            value={searchQuery} 
+            value={searchQuery}
             onChange={handleSearchChange}
           />
         </div>
 
-        { showResults && (
-          <div 
-          className="absolute mt-2 w-full p-2 bg-white shadow-lg rounded-b max-h-56 overflow-y-auto z-[1]"
-          style = {{
-            top: 60 + "px",
-          }}
+        {showResults && (
+          <div
+            className="absolute mt-2 w-full p-2 bg-white shadow-lg rounded-b max-h-56 overflow-y-auto z-[1]"
+            style={{
+              top: 60 + "px",
+            }}
           >
             {results.map((item, index) => {
               return (
-                <div 
+                <div
                   key={item._id}
                   onMouseDown={() => handleSelection(index)}
                   ref={index === focusedIndex ? resultContainer : null}
                   style={{
-                    backgroundColor: index === focusedIndex ? "rgba(0,0,0,0.1)" : "",
+                    backgroundColor:
+                      index === focusedIndex ? "rgba(0,0,0,0.1)" : "",
                   }}
-                className="cursor-pointer hover:bg-black hover:bg-opacity-10 p-2"
+                  className="cursor-pointer hover:bg-black hover:bg-opacity-10 p-2"
                 >
-                  {renderItem(item)} 
+                  {renderItem(item)}
                 </div>
               );
             })}
-            </div> 
-            )}
+          </div>
+        )}
         <div className="dropdown dropdown-end">
           <div
             tabIndex={0}
@@ -166,7 +169,7 @@ export default function Navbar() {
             className="btn btn-ghost btn-circle avatar"
           >
             <div className="w-10 rounded-full ">
-              <img 
+              <img
                 alt="Tailwind CSS Navbar component"
                 src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
               />
@@ -204,18 +207,23 @@ export default function Navbar() {
             )}
           </ul>
         </div>
-        
+
         {isAuthenticated ? (
-          <button onClick={handleLogout} className="btn btn-primary bg-blue-300 hover:bg-sky-500 border-gray-600 ">
+          <button
+            onClick={handleLogout}
+            className="btn btn-primary bg-blue-300 hover:bg-sky-500 border-gray-600 "
+          >
             Sign Out
           </button>
         ) : (
-          <Link to="/login" className="btn btn-primary bg-sky-300 hover:bg-sky-500 border-gray-800">
+          <Link
+            to="/login"
+            className="btn btn-primary bg-sky-300 hover:bg-sky-500 border-gray-800"
+          >
             Login
           </Link>
         )}
       </div>
     </div>
   );
-  
 }
