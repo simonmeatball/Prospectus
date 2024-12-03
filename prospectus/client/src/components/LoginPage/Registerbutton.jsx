1cf3c7fcffeb137c3c2ce089ed2fa5005454840b
jasonni <jasonni19@g.ucla.edu>
2024-12-02 22:00:31 -0800

    merge

--- prospectus/client/src/components/LoginPage/Registerbutton.jsx
+++ prospectus/client/src/components/LoginPage/Registerbutton.jsx
@@ -4,15 +4,6 @@ import { Link } from "react-router-dom"
 
 function Registerbutton() {
     return (
-<<<<<<< HEAD
-        <div className="mt-3 flex">
-            <p> Don't have account? </p>
-
-            <Link to="/" className="ml-2 underline"> Sign up.</Link>
-        </div>)
-}
-
-=======
         <div className="mt-3 flex ">
             <p> Don't have account? </p>
 
@@ -21,5 +12,4 @@ function Registerbutton() {
 }
 
 
->>>>>>> 00948be9e99ec462a22dc78482578a02304d0dc7
 export default Registerbutton
